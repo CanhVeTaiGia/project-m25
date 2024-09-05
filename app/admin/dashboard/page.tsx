77dcc24d6d9aeef66e39789f1b206b367b42ee82
CanhVeTaiGia <lvy16012005@gmail.com>
2024-09-05 09:29:22 +0700

    add

---
+++ app/admin/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import AdminHeader from "@/layouts/header/AdminHeader";
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div className="w-[100%] h-[100vh]">
+      <AdminHeader></AdminHeader>
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ layouts/header/AdminHeader.tsx
@@ -0,0 +1,9 @@
+
+
+const AdminHeader = () => {
+  return (
+    <div className="w-[100%] h-[100px] text-[blue] bg-black">hello</div>
+  );
+};
+
+export default AdminHeader;
